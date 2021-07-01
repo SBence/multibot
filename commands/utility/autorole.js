56f9ea9da1b5521423f4a0bc3333c1be6b921b66
SBence <43538202+SBence@users.noreply.github.com>
2021-07-01 20:01:38 +0200

    Prepare guild db model and command for autorole

---
+++ commands/utility/autorole.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'autorole',
+    description: '',
+    runInDMs: false,
+    protected: true, // TODO Needed?
+    async run(message, args, bot) {
+        return;
+    }
+};
--- models/Guilds.js
+++ models/Guilds.js
@@ -5,17 +5,38 @@ module.exports = database.define('guilds', {
         type: DataTypes.STRING,
         primaryKey: true
     },
+    members: {
+        type: DataTypes.STRING,
+        get() { return this.getDataValue('members').split(';'); },
+        set(val) { this.setDataValue('members', val.join(';')); }
+    },
     prefix: {
         type: DataTypes.STRING,
         defaultValue: 'm.',
         allowNull: false
     },
-    bulkdelete: {
+    autorole: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
         allowNull: false
     },
+    givenrole: {
+        type: DataTypes.STRING
+    },
+    botautorole: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    },
+    givenbotrole: {
+        type: DataTypes.STRING
+    },
     avatar: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    },
+    bulkdelete: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
         allowNull: false
